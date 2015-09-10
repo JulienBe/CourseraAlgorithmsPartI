876dacd13bd2dd7e0e8eae78f4369e03c89b6167
JulienBe <bertozzijulien@gmail.com>
2015-09-10 21:47:22 +0200

    Initial commit

---
+++ src/com/company/Percolation.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Percolation {
+
+    private final Cell[][] cells;
+
+    public Percolation(int N) {
+        this.cells = new Cell[N][N];
+    }
+
+    public static void main(String[] args) {
+    }
+}
+
+class Cell {}
